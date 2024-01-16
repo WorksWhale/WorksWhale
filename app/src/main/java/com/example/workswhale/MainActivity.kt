a63a6684b444f2f9041a3a3f5e5f7d9cabee6b9a
phs8505 <phs8505@gmail.com>
2024-01-16 10:30:14 +0900

    Add : 탭 영역 프래그먼트 전환 기능
    1) FrameLayout을 ViewPager2로 변경
    
    2) 메인 액티비티의 탭 아이템의 아이디가 존재하면 에러가 떠서 아이디를 없앰
    
    3) ViewPagerAdapter를 만듬
    
    4) TabLayoutMediator를 통해 TabLayout과 ViewPager를 연결시킴
    
    5) 탭 아이템의 텍스트를 TabLayoutMediator에서 정해줌
    
    6) 연락처 추가 프래그먼트는 탭바를 통해 접근하는 것이 아니기에 추가시키지 않음

--- app/src/main/java/com/example/workswhale/MainActivity.kt
+++ app/src/main/java/com/example/workswhale/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.workswhale
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.workswhale.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val adapter = ViewPagerAdapter(this)
+        binding.viewPagerMain.adapter = adapter
+
+        TabLayoutMediator(binding.tabLayoutBottom, binding.viewPagerMain) { tab, position ->
+            when (position) {
+                0 -> tab.text = "연락처"
+                1 -> tab.text = "마이 페이지"
+            }
+        }.attach()
     }
 }
