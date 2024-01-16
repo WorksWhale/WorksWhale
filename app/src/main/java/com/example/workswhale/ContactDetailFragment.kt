0624a939741df932310e62770cc9e2495e182f9c
rihan530 <renovatio530@gmail.com>
2024-01-16 18:56:33 +0900

    상세정보에서 SMS 및 전화걸기

--- app/src/main/java/com/example/workswhale/ContactDetailFragment.kt
+++ app/src/main/java/com/example/workswhale/ContactDetailFragment.kt
@@ -1,11 +1,27 @@
 package com.example.workswhale
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
+import androidx.fragment.app.Fragment
+import com.example.workswhale.databinding.FragmentContactDetailBinding
+
+
 class ContactDetailFragment : Fragment() {
+
+    private var binding: FragmentContactDetailBinding? = null
+    lateinit var resultLauncher: ActivityResultLauncher<Intent>
+
+//    private val receivedItem: Contact?
+//        get() = intent.getParcelableExtra("Contact")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,10 +32,28 @@ class ContactDetailFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_detail, container, false)
+        binding = FragmentContactDetailBinding.inflate(inflater, container, false)
+
+        val phoneNumber = binding!!.tvDetailPhoneNumber.text//phonNumber에는 010-1234-5678로 넣으면 01012345678로 변환됨
+
+        with(binding) {
+            this!!.tvMessage.setOnClickListener {
+                val smsUri = Uri.parse("smsto:$phoneNumber")
+                val intent = Intent(Intent.ACTION_SENDTO)
+                intent.setData(smsUri)
+                intent.putExtra("sms_body", "") //해당 값에 전달하고자 하는 문자메시지 전달
+                startActivity(intent)
+            }
+
+            this.tvCall.setOnClickListener {
+                val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                startActivity(intent)
+            }
+        }
+
+        return binding?.root
     }
 
     companion object {
