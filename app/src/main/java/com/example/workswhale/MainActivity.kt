e2dd5815cdbd14fc627f3c85284f1979208bbc43
CodeNewbieee <choi2232@hanmail.net>
2024-01-16 11:40:27 +0900

    Design : 레이아웃 코드 정리

--- app/src/main/java/com/example/workswhale/MainActivity.kt
+++ app/src/main/java/com/example/workswhale/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.workswhale
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.workswhale.databinding.ActivityMainBinding
+import com.example.workswhale.databinding.FragmentContactListBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().replace(R.id.framelayout_main, ContactListFragment()).commit()
 
     }
 }
